3f3cd935f0553443379fc3647c2ce24e0c38836f
jessie-backports <57424611+jessie-backports@users.noreply.github.com>
2020-10-23 08:42:01 +0800

    Update ridge_regression.py

--- ridge_regression.py
+++ ridge_regression.py
@@ -17,7 +17,7 @@ class RidgeRegression(Graph):
     """
     def __init__(self, alpha=1.0, learning_rate=0.05, epoch_num=100, batch_size='auto'):
         # modify self.skip = False to run the extra test for bonus question
-        self.skip = True
+        self.skip = False
         self.alpha = alpha
         super().__init__(learning_rate=learning_rate, epoch_num=epoch_num, batch_size=batch_size)
         pass
